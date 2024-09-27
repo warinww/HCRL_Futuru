db190eeff62be03a2f74296cc288d40f7353d96b
warinww <warinrinrin14@gmail.com>
2024-09-27 23:47:24 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:project_hcrl/page/FirstPage.dart';
-// import 'package:project_hcrl/page/home.dart';
-// ignore: duplicate_import
-import 'package:project_hcrl/page/FirstPage.dart';
-
 void main() {
   runApp(const MainApp());
 }
